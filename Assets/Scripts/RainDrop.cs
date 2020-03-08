b81f7fac41d221a8ab4e99d2fb3a6d249a2c4167
Andrew Nguyen Vo <awnguy01@louisville.edu>
2020-03-08 03:38:47 -0400

    Finished project commit

---
+++ Assets/Scripts/RainDrop.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainDrop : MonoBehaviour
+{
+    private float acceleration;
+    private float spinSpeed;
+    private Vector3 spinAngle;
+
+    private float minAcceleration = .1f;
+    private float maxAcceleration = .25f;
+    private float minSpinSpeed = 25f;
+    private float maxSpinSpeed = 50f;
+
+    private float secondsCounter = 0f;
+
+    private void Awake()
+    {
+        acceleration = Random.Range(minAcceleration, maxAcceleration);
+        spinSpeed = Random.Range(minSpinSpeed, maxSpinSpeed);
+        spinAngle = new Vector3(0, Random.Range(0, 1), 0);
+
+        SetScale();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spin();
+        Fall();
+        DestroyIfGrounded();
+    }
+
+    void Fall()
+    {
+        secondsCounter += Time.deltaTime;
+        float velocity = secondsCounter * acceleration;
+        transform.Translate(Vector3.down * velocity);
+    }
+
+    void Spin()
+    {
+        transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+    }
+
+    void SetScale()
+    {
+        float scaleFactor = Random.Range(0, 3);
+        transform.localScale += new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+    }
+
+    void DestroyIfGrounded()
+    {
+        Camera camera = Camera.main;
+        if (camera.WorldToViewportPoint(gameObject.transform.position).y <= -5f)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RainManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainManager : MonoBehaviour
+{
+    public GameObject rainDrop;
+    public int dropsPerSecond;
+    private float leftSpawnBound = -30f;
+    private float rightSpawnBound = 30f;
+    private float minSpawnHeight = 30f;
+    private float maxSpawnHeight = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("RenderDrops", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void RenderDrops()
+    {
+        for (int i = 0; i < dropsPerSecond; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(leftSpawnBound, rightSpawnBound), Random.Range(minSpawnHeight, maxSpawnHeight), 5);
+            Instantiate(rainDrop, position, Quaternion.identity);
+        }
+    }
+}
